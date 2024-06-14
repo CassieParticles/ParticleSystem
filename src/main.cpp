8283374b1061c6bc1c418b0ab9b0e444cfa2f77f
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2024-06-14 21:29:24 +0100

    Added Window, Device and render target objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+#include <engine/Engine/Window.h>
 
 int main()
 {
+	Window* window = Window::InitializeWindow("Test window", 800, 800);
+
+	window->bindRTV();
+
+	while (!window->getWindowShouldClose())
+	{
+		window->clearBackBuffer(DirectX::XMFLOAT4(0.2, 0.8, 0.6, 0.4));
+		std::cout << "Window actions\n";
+		window->presentBackBuffer();
+	}
+
+	std::cout << "Test\n";
 	return 0;
 }
