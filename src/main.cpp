909bf18f8251c09ea6f54820fcc9de792fb74e6e
Cassie Parsons <71268741+CassieParticles@users.noreply.github.com>
2024-07-08 16:33:29 +0100

    Added input reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <engine/Engine/Window.h>
+#include <engine/Engine/TimeManager.h>
 
 #include <engine/D3DObjects/Device.h>
 #include <engine/D3DObjects/Mesh.h>
@@ -15,6 +16,7 @@ int main()
 	Window* window = Window::InitializeWindow("Test window", 800, 800);
 	Device* device = Device::Instance();
 	CBufferManager* cBufferManager = CBufferManager::Instance();
+	TimeManager timeManager;
 
 	float vertices[9] =
 	{
@@ -30,6 +32,11 @@ int main()
 	float offset[3] =
 	{
 		0.25f,0.0f,0.0f
+	};
+
+	float newOffset[3] =
+	{
+		0.0f,1.f,0.0f
 	};
 
 	Pipeline pipeline;
@@ -49,16 +56,27 @@ int main()
 
 	pipeline.compilePipeline();
 
-	cBufferManager->addBuffer("offset", offset, false, 16);
+	cBufferManager->addBuffer("offset", offset, true, 16);
 
 	pipeline.bindConstantBuffer("offset", Shaders::VERTEX_SHADER, 0);
 
+	
+
+	timeManager.Start();
 	while (!window->getWindowShouldClose())
 	{
+		//Update buffers
+		timeManager.Tick();
+
+		newOffset[0] = cos(timeManager.ElapsedTime()) * 0.5f;
+		newOffset[1] = sin(timeManager.ElapsedTime()) * 0.5f;
+
+		cBufferManager->getCBuffer("offset")->updateCBuffer(newOffset, 16);
+
+		//Render to the screen
 		window->bindRTV();
 
 		window->clearBackBuffer(DirectX::XMFLOAT4(0.2, 0.4, 0.6, 0.4));
-
 
 		pipeline.bind();
 		mesh.setBuffers();
